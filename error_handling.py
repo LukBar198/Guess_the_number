aa7b9ecd96b42b703a9fd5d50f06fa02fe40437a
LukBar198 <lukasz.baran191@gmail.com>
2023-03-27 22:03:03 +0200

    Initial commit

---
+++ error_handling.py
@@ -0,0 +1,20 @@
+def exception_handler(func):
+    """Error handling decorator
+
+    :param func: function passed to decorator
+    :return:     function with added exception handling
+    """
+
+    def inner_function(*args, **kwargs):
+        """
+
+        :param args:    positional arguments
+        :param kwargs:  key arguments
+        :return:        exception handling
+        """
+        try:
+            func(*args, **kwargs)
+        except TypeError:
+            print(f"{func.__name__} only takes numbers as the argument")
+
+    return inner_function
